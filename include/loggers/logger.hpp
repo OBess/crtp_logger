81bc70e959b950f458ece615a2cbddf9496ce6a9
OBess <dima.suschenko03@gmail.com>
2021-06-30 18:39:34 +0300

    Added CRTP and two template methods

--- include/loggers/logger.hpp
+++ include/loggers/logger.hpp
@@ -2,21 +2,24 @@
 #ifndef LOGGER_HPP
 #define LOGGER_HPP
 
+template <class Derived>
 class logger
 {
 public:
    virtual ~logger() = default;
 
-   void Log(const std::string &message)
+   template<typename ... Args>
+   void Log(const Args &message)
    {
-      this->open();
-      this->write(message);
-      this->close();
+      Derived->open();
+      Derived->write(message);
+      Derived->close();
    }
 
 protected:
    virtual void open() = 0;
-   virtual void write(const std::string &message) = 0;
+   template<typename ... Args>
+   virtual void write(const Args &message) = 0;
    virtual void close() = 0;
 };
 
